08d48dc0586a302091bde67a79f5cd4963452ffc
Sakshi Pareek <sakshipareek505@gmail.com>
2025-03-26 16:20:44 +0530

    add herosection again

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import NavBar from "../common/NavBar";
-// import MainHeader from "../MainHeader";
+import MainHeader from "../MainHeader";
 import AboutUs from "../AboutUs";
 import Footer from "../common/Footer";
 import HomeProduct from "../HomeProduct";
 import Faqs from "../Faqs";
 import ProductCategory from "../ProductCategory";
-import EventBanner from "../common/EventBanner";
+// import EventBanner from "../common/EventBanner";
 import CountdownPopup from "../common/CountdownPopup";
 
 const HomePage = () => {
@@ -22,8 +22,8 @@ const HomePage = () => {
       <div>
         <NavBar />
         <div className="overflow-hidden">
-          <EventBanner />
-          {/* <MainHeader /> */}
+          {/* <EventBanner /> */}
+          <MainHeader />
           <AboutUs />
           <ProductCategory />
           <HomeProduct />
