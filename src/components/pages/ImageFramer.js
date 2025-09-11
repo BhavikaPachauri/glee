0bac15b77c93ab2944f2caa0613b6074daf78bbf
BhavikaPachauri <pachauribhavika2000@gmail.com>
2025-09-11 12:48:42 +0530

    glee

--- src/components/pages/ImageFramer.js
+++ src/components/pages/ImageFramer.js
@@ -126,7 +126,7 @@ const ImageFramer = () => {
 
                 {/* Frame overlay */}
                 <img
-                  src="/Frame.png"
+                  src="./Frame.png"
                   alt="Frame"
                   className="absolute pointer-events-none z-20 w-full"
                   style={{
